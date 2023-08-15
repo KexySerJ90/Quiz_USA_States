dc8b5139925d48a7be40bd27e7373cb03c8844b6
KexySerJ90 <exserj9011@gmail.com>
2023-08-15 20:35:43 +0300

    Begin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen=turtle.Screen()
 screen.title('U.S.States Game')
@@ -8,4 +9,14 @@ turtle.shape(image)
 
 #Pop up a dialog window for input of a string.Arguments: title is the title of the dialog window, prompt is a text mostly describing what information to input.
 answer_state=screen.textinput(title='Guess the State', prompt="What's another state name?")
-print(answer_state)
+data=pandas.read_csv('50_states.csv')
+all_states=data.state.to_list()
+score=0
+if answer_state.lower() in all_states or answer_state in all_states :
+        score+=1
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data=data[data.state==answer_state]
+        t.goto(state_data.x,state_data.y)
+        t.write(state_data.state)
