d73667cc686b965e57976b29ff7e7adbfb4473b5
KexySerJ90 <exserj9011@gmail.com>
2023-08-15 21:53:20 +0300

    Get score

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import turtle
 import pandas
+import subprocess
 
+# Создаем экран и загружаем изображение штатов
 screen = turtle.Screen()
 screen.title('U.S.States Game')
 image = 'blank_states_img.gif'
 turtle.addshape(image)
 turtle.shape(image)
 
-# Pop up a dialog window for input of a string.Arguments: title is the title of the dialog window, prompt is a text mostly describing what information to input.
-
+# Загружаем данные о штатах из файла CSV
 data = pandas.read_csv('50_states.csv')
 all_states = data.state.to_list()
+
+# Создаем пустой набор для хранения отгаданных штатов
 score = set()
+
+# Запускаем игру до тех пор, пока не будут отгаданы все 50 штатов или пользователь не введет 'Exit'
 while len(score) < 50:
+    # Получаем ответ пользователя и проверяем его на корректность
     answer_state = screen.textinput(title=f'Guess the State: {len(score)}/50',
                                     prompt="What's another state name? or write 'Exit'").title()
     if answer_state == 'Exit':
+        # Если пользователь ввел 'Exit', сохраняем список незадействованных штатов в файл CSV и открываем его в приложении Numbers
         missing_state = [i for i in all_states if i not in score]
-        df=pandas.DataFrame({'Missing state': missing_state})
+        df = pandas.DataFrame({'Missing state': missing_state})
         df.to_csv('missing_state.csv')
+        subprocess.Popen(['open', '-a', 'Numbers.app', 'missing_state.csv'])
         break
     if answer_state in all_states:
+        # Если пользователь ввел правильное название штата, добавляем его в набор отгаданных штатов и отмечаем его на карте
         score.add(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
